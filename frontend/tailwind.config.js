1000dc12591be5d75fdcdf4399cef58b58872b1d
Travis Rodriguez <travisgolembiewski@gmail.com>
2025-11-15 01:38:12 -0500

    smh big break in css tailwind use

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,12 +5,7 @@ export default {
       "./src/**/*.{js,jsx}",
     ],
     theme: {
-      extend: {
-        colors: {
-          primary: '#1e40af',
-          secondary: '#0f172a',
-        }
-      },
+      extend: {},
     },
     plugins: [],
   }
